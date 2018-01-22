d33315ad647fd7f1f92abf2923736bc942967fea
Frank Denis <github@pureftpd.org>
2018-01-22 18:05:41 +0100

    Add support for stamps

--- main.c
+++ main.c
@@ -244,6 +244,101 @@ parse_cert_files(struct context *c)
     return 0;
 }
 
+#ifndef sodium_base64_VARIANT_URLSAFE_NO_PADDING
+#define EQ(x, y) \
+    ((((0U - ((unsigned int) (x) ^ (unsigned int) (y))) >> 8) & 0xFF) ^ 0xFF)
+#define GT(x, y) ((((unsigned int) (y) - (unsigned int) (x)) >> 8) & 0xFF)
+#define GE(x, y) (GT(y, x) ^ 0xFF)
+#define LT(x, y) GT(y, x)
+
+static int
+b64_byte_to_urlsafe_char(unsigned int x)
+{
+    return (LT(x, 26) & (x + 'A')) |
+           (GE(x, 26) & LT(x, 52) & (x + ('a' - 26))) |
+           (GE(x, 52) & LT(x, 62) & (x + ('0' - 52))) | (EQ(x, 62) & '-') |
+           (EQ(x, 63) & '_');
+}
+
+char *
+sodium_bin2base64(char * const b64, const size_t b64_maxlen,
+                  const unsigned char * const bin, const size_t bin_len,
+                  const int variant)
+{
+    size_t       acc_len = (size_t) 0;
+    size_t       b64_len;
+    size_t       b64_pos = (size_t) 0;
+    size_t       bin_pos = (size_t) 0;
+    size_t       nibbles;
+    size_t       remainder;
+    unsigned int acc = 0U;
+
+    nibbles = bin_len / 3;
+    remainder = bin_len - 3 * nibbles;
+    b64_len = nibbles * 4;
+    if (remainder != 0) {
+        b64_len += 2 + (remainder >> 1);
+    }
+    if (b64_maxlen <= b64_len) {
+        exit(1);
+    }
+    while (bin_pos < bin_len) {
+        acc = (acc << 8) + bin[bin_pos++];
+        acc_len += 8;
+        while (acc_len >= 6) {
+            acc_len -= 6;
+            b64[b64_pos++] = (char) b64_byte_to_urlsafe_char((acc >> acc_len) & 0x3F);
+        }
+    }
+    if (acc_len > 0) {
+        b64[b64_pos++] = (char) b64_byte_to_urlsafe_char((acc << (6 - acc_len)) & 0x3F);
+    }
+    do {
+        b64[b64_pos++] = 0U;
+    } while (b64_pos < b64_maxlen);
+
+    return b64;
+}
+#endif
+
+static char *create_stamp(const char *resolver_address, const unsigned char *provider_publickey,
+                          const char *provider_name, bool dnssec, bool nolog)
+{
+    unsigned char *stamp_bin, *p;
+    char *stamp;
+    unsigned char props[8] = {0};
+    size_t len;
+    size_t resolver_address_len = strlen(resolver_address),
+           provider_publickey_len = crypto_sign_ed25519_PUBLICKEYBYTES,
+           provider_name_len = strlen(provider_name);
+
+    if (dnssec)
+        props[0] |= 1;
+    if (nolog)
+        props[1] |= 2;
+    len = 1 + 8 + 1 + resolver_address_len + 1 + provider_publickey_len + 1 + provider_name_len;
+    if ((stamp_bin = malloc(len)) == NULL)
+        exit(1);
+    p = stamp_bin;
+    *p++ = 0x01;
+    memcpy(p, props, sizeof props); p += sizeof props;
+    *p++ = (unsigned char) resolver_address_len;
+    memcpy(p, resolver_address, resolver_address_len); p += resolver_address_len;
+    *p++ = (unsigned char) provider_publickey_len;
+    memcpy(p, provider_publickey, provider_publickey_len); p += provider_publickey_len;
+    *p++ = (unsigned char) provider_name_len;
+    memcpy(p, provider_name, provider_name_len); p += provider_name_len;
+    if (p - stamp_bin != len) {
+        exit(1);
+    }
+    if ((stamp = malloc(len * 4 / 3 + 2)) == NULL) {
+        exit(1);
+    }
+    sodium_bin2base64(stamp, len * 4 / 3 + 2, stamp_bin, len, 7);
+    free(stamp_bin);
+    return stamp;
+}
+
 int
 main(int argc, const char **argv)
 {
@@ -258,6 +353,7 @@ main(int argc, const char **argv)
     int provider_publickey_dns_records = 0;
     int verbose = 0;
     int use_xchacha20 = 0;
+    int nolog = 0, dnssec = 0;
     struct argparse argparse;
     struct argparse_option options[] = {
         OPT_HELP(),
@@ -281,6 +377,8 @@ main(int argc, const char **argv)
         OPT_STRING(0, "crypt-secretkey-file", &c.crypt_secretkey_file,
                    "crypt secret key file (default: ./crypt_secret.key)"),
         OPT_INTEGER(0, "cert-file-expire-days", &cert_file_expire_days, "cert file expire days (default: 365)"),
+        OPT_BOOLEAN(0, "nolog", &nolog, "Indicate that the server doesn't store logs"),
+        OPT_BOOLEAN(0, "dnssec", &dnssec, "Indicate that the server supports DNSSEC"),
         OPT_STRING('a', "listen-address", &c.listen_address,
                    "local address to listen (default: 0.0.0.0:53)"),
         OPT_STRING('r', "resolver-address", &c.resolver_address,
@@ -332,19 +430,44 @@ main(int argc, const char **argv)
     if (gen_provider_keypair) {
         uint8_t provider_publickey[crypto_sign_ed25519_PUBLICKEYBYTES];
         uint8_t provider_secretkey[crypto_sign_ed25519_SECRETKEYBYTES];
+
+        if (!dnssec) {
+            fprintf(stderr,
+                    "Warning: do not forget to add --dnssec if your server supports DNSSEC\n\n");
+        }
+        if (!nolog) {
+            fprintf(stderr,
+                    "Warning: do not forget to add --nolog if your server doesn't store logs\n\n");
+        }
+        if (!c.provider_name) {
+            fprintf(stderr,
+                    "Missing provider name. Ex: --provider-name=2.dnscrypt-cert.example.com\n");
+            exit(1);
+        }
+        if (!c.resolver_address) {
+            fprintf(stderr,
+                    "Missing resolver address. Ex: --resolver-address=192.168.1.1\n");
+            exit(1);
+        }
+
         printf("Generate provider key pair...");
         fflush(stdout);
-        if (crypto_sign_ed25519_keypair(provider_publickey, provider_secretkey)
-            == 0) {
-            printf(" ok.\n");
+        if (crypto_sign_ed25519_keypair(provider_publickey, provider_secretkey) == 0) {
             char fingerprint[80];
+            char *stamp;
+
+            printf(" ok.\n");
+            stamp = create_stamp(c.resolver_address, provider_publickey, c.provider_name, dnssec, nolog);
             dnscrypt_key_to_fingerprint(fingerprint, provider_publickey);
-            printf("Provider public key: %s\n\n", fingerprint);
-            printf("This is the provider key you should give to users for your service.\n"
-                   "(i.e. dnscrypt-proxy --provider-key=%s\n"
-                   "                     --resolver-address=<your resolver public IP>\n"
-                   "                     --provider-name=2.dnscrypt-cert...)\n",
-                   fingerprint);
+
+            printf("Stamp for dnscrypt-proxy 2.x:\n"
+                   "  sdns://%s\n\n"
+                   "Parameters for dnscrypt-proxy 1.x:\n"
+                   "  dnscrypt-proxy --provider-key=%s\n"
+                   "                 --resolver-address=%s\n"
+                   "                 --provider-name=%s\n",
+                   stamp,
+                   fingerprint, c.resolver_address, c.provider_name);
             if (write_to_file
                 (c.provider_publickey_file, (char *)provider_publickey,
                  crypto_sign_ed25519_PUBLICKEYBYTES) == 0
